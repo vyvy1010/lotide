5bc48bfd7442eb6c5d8b7dd5c7af87a02ba5fff9
Vy Pham <vyphamt92@email.com>
2023-06-17 23:21:21 -0600

    Add assertEqual for console.assert

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😃' + "Assertion passed: " + actual + " === " + expected);
+
+  } else {
+    console.log('❌' + "Assertion failed: " + actual + " !== " + expected);
+
+  }
+
+};
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
