4da536b97620e8ab0e4e1a468265d9dd1e0f125d
Vy Pham <vyphamt92@email.com>
2023-06-22 00:20:39 -0600

    add countOnly

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+// assertEqual FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion failed: ${actual}!=== ${expected}`);
+  }
+};
+
+// countOnly FUNCTION
+// inside the loop,
+//  if item in itemsToCount
+//   increment the counter for each item:
+//    set a property with that string key to:
+//     the value that was already there (or zero if nothing there) with 1 added to it.
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (!results[item]) {
+        results[item] = 1;
+      } else {
+        results[item] += 1;
+      }
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
