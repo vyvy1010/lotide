e9f05abcb97a396482c4c33f1a587bbe824f0c0c
Vy Pham <vyphamt92@email.com>
2023-07-09 01:27:48 -0600

    modified tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,5 @@
 const assertEqual = require('./assertEqual');
-// const assertEqual = function(actual, expected) {
-//   if (actual.length === expected.length) {
-//     for (let i = 0; i < actual.length; i++) {
-//       if (actual[i] !== expected[i]) {
-//         return `❌ Assertion failed: ${actual}!=== ${expected}`;
-//       }
-//     }
-//     return `😃 Assertion passed: ${actual} === ${expected}`;
-//   }
-//   else {
-//     return `❌ Assertion failed: ${actual}!=== ${expected}`;
 
-//   }
-// };
 const tail = function(array) {
   return array.slice(1);
 };
