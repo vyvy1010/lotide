2dda47e8910808241ae150a6ae52923f497867d4
Vy Pham <vyphamt92@email.com>
2023-07-09 01:41:11 -0600

    modified assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -18,5 +7,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`❌ Assertion failed: ${actual}!=== ${expected}`);
   }
 };
-// Test case:
-assertArraysEqual([1,2,3], [1,2,3]);
+
+module.exports = assertArraysEqual;
