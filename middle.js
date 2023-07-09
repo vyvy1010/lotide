e0219e7db3f711aca12d09200c2677032430ad39
Vy Pham <vyphamt92@email.com>
2023-07-09 01:46:27 -0600

    modified middle.js

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`😃 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion failed: ${actual}!=== ${expected}`);
-  }
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let middleArray = [];
   const l = array.length;
@@ -37,10 +14,4 @@ const middle = function(array) {
   return middleArray;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
